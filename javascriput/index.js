03757b268aec873fcbbf22433c7fa0f07880b601
i.k <121545103+I-kumi@users.noreply.github.com>
2023-01-05 22:44:33 +0900

    完成

--- javascriput/index.js
+++ javascriput/index.js
@@ -1,90 +1,119 @@
+// ページがロードされたら要素をフェードインさせる
+$(window).on('load',function(){
+    setTimeout(function(){
+		$('.l-header, .l-main__logoBox, .l-main__arrow').fadeIn(1000);
+	},1000);
+});
+
+//メインページがheader部分までスクロールされたら背景色をつけるクラスを付与
 $(window).on('scroll', function () {
     if (765 < $(this).scrollTop()) {
-        $('.l-header__nav').addClass('change-color');
+        $('.l-header__nav').addClass('js-background');
     } else {
-        $('.l-header__nav').removeClass('change-color');
+        $('.l-header__nav').removeClass('js-background');
+    }
+});
+
+//header部分に背景色が付いたら文字色を切り替えるクラスを付与
+$(window).on('scroll', function () {
+    if (765 < $(this).scrollTop()) {
+        $('.l-header__navLink').addClass('js-textColor');
+    } else {
+        $('.l-header__navLink').removeClass('js-textColor');
     }
 });
 
 $(window).on('scroll', function () {
     if (765 < $(this).scrollTop()) {
-        $('.l-header__navLink').addClass('textColor');
+        $('.l-header__langList').addClass('js-textColor');
     } else {
-        $('.l-header__navLink').removeClass('textColor');
+        $('.l-header__langList').removeClass('js-textColor');
     }
 });
 
+//メインページがheader部分までスクロールされた時に▼の色を変更する
 $(window).on('scroll', function () {
     if (765 < $(this).scrollTop()) {
-        $('.l-header__lang li').addClass('listBackground');
+        $('.l-header__navLang').removeClass('js-triangle');
     } else {
-        $('.l-header__lang li').removeClass('listBackground');
+        $('.l-header__navLang').addClass('js-triangle');
     }
 });
 
+//メインページがheader部分までスクロールされたらロゴ画像を表示する
 $(window).on('scroll', function () {
     if (765 < $(this).scrollTop()) {
-        $('.abc').addClass('textColor');
+        $('.l-header__img').removeClass('js-appear');
     } else {
-        $('.abc').removeClass('textColor');
+        $('.l-header__img').addClass('js-appear');
     }
 });
 
+// ドロップダウンメニュー
+$(function() {
+    //.dropdown-menuを一旦隠す
+    $('.l-header__langItem').find('.l-header__lang').hide();
+    //.menuをhoverした場合
+    $('.l-header__langItem').hover(function(){
+        //.dropdown-menuをslideDown
+        $(".l-header__lang:not(:animated)", this).slideDown();
+        //hoverが外れた場合
+    }, function(){
+        //.dropdown-menuをslideUp
+        $(".l-header__lang",this).slideUp();
+    });
+});
+
+//header部分に背景色が付いたらドロップダウンメニュー部分に背景色をつけるクラスを付与
 $(window).on('scroll', function () {
     if (765 < $(this).scrollTop()) {
-        $('.l-header__image').removeClass('l-header__animate');
+        $('.l-header__lang li').addClass('js-listBackground');
     } else {
-        $('.l-header__image').addClass('l-header__animate');
+        $('.l-header__lang li').removeClass('js-listBackground');
     }
 });
 
 
-
+//スクロールで画像の色切り替え
 $(function () {
     $(window).on('scroll', function () {
         var wh = $(window).height();
-        if ($('.hoge1-js').offset().top - wh < $(this).scrollTop()) {
-            $('.hogehoge1-js').addClass('grayscale');
+        if ($('.js-getHeight1').offset().top - wh < $(this).scrollTop()) {
+            $('.js-coloring1').addClass('js-grayscale');
         }
-        if ($('.hoge2-js').offset().top - wh < $(this).scrollTop()) {
-            $('.hogehoge2-js').addClass('grayscale');
+        if ($('.js-getHeight2').offset().top - wh < $(this).scrollTop()) {
+            $('.js-coloring2').addClass('js-grayscale');
         }
-        if ($('.hoge3-js').offset().top - wh < $(this).scrollTop()) {
-            $('.hogehoge3-js').addClass('grayscale');
+        if ($('.js-getHeight3').offset().top - wh < $(this).scrollTop()) {
+            $('.js-coloring3').addClass('js-grayscale');
         }
-        if ($('.hoge4-js').offset().top - wh < $(this).scrollTop()) {
-            $('.hogehoge4-js').addClass('grayscale');
+        if ($('.js-getHeight4').offset().top - wh < $(this).scrollTop()) {
+            $('.js-coloring4').addClass('js-grayscale');
         }
-        if ($('.hoge5-js').offset().top - wh < $(this).scrollTop()) {
-            $('.hogehoge5-js').addClass('grayscale');
+        if ($('.js-getHeight5').offset().top - wh < $(this).scrollTop()) {
+            $('.js-coloring5').addClass('js-grayscale');
         }
     });
 });
 
-//ドロップダウンのホバーイベント
-$(function() {
-    $("ul.l-header__langList li").hover(function() {
-            $(".l-header__lang:not(:animated)", this).slideDown();
-        }, function() {
-            $(".l-header__lang", this).slideUp();
-        });
-});
-
+//ナビゲーションメニューをクリックでモーダルの表示切り替えをする
 $(function() {
     $('.l-header__boarder').on('click',function(){
-        $('.l-header__boarder').toggleClass('panelactive');
+        $('.l-header__boarder').toggleClass('js-active');
         $('.l-header__menu').fadeToggle();
     });
 });
 
+//ナビゲーションメニューをクリックでboarderを×にする
 $(function() {
-    $('.l-header__boarder').on('click',function(){
-        $('.l-header__textBox').toggleClass('backGround');
+    $('.l-header__boarder').click(function() {
+        $('.l-header__boarder').toggleClass('js-bolder');
     });
 });
 
+//ナビゲーションメニューをクリックでモーダルに背景色をつけるクラスを付与
 $(function() {
-    $('.l-header__boarder').click(function() {
-        $('.l-header__boarder').toggleClass('changeBolder');
+    $('.l-header__boarder').on('click',function(){
+        $('.l-header__textBox').toggleClass('js-modalBackground');
     });
 });
